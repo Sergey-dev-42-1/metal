e4f2f78c0f9aea5d66fd86800fc749e40dafe9da
Ryzhikh_Sergei <ryzhih-sergei@bauart.pro>
2024-09-01 23:14:26 +0700

    Remove dangerous dereferencings

--- internal/pkg/domain/repositories/repository.go
+++ internal/pkg/domain/repositories/repository.go
@@ -40,7 +40,6 @@ func (m *MemStorage) CreateOrUpdate(metric models.Metrics) models.Metrics {
 			m.Metrics[name] = metric
 			return metric
 		}
-		fmt.Println("test",*m.Metrics[name].Delta, *metric.Delta)
 		newDelta := *m.Metrics[name].Delta + *metric.Delta
 		m.Metrics[name] = models.Metrics{
 			Delta: &newDelta,
